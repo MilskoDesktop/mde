cf70804aa21f8f3b66b7a48f426f0a5e4a39c590
NishiOwO <nishi@nishi.boats>
2025-11-25 05:32:58 +0900

    update

--- format.sh
+++ format.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 format() {
 	echo "-- $1"
+	old="`pwd`"
 	cd $1
 	clang-format --verbose -i `find $2 -name "*.c" -or -name "*.h"`
-	cd ..
+	cd "$old"
 }
 
 format mdebase "mdm milkwm mpanel"
-format mdelibs "audio core include"
+format mdelibs "audio audio/pthread core sound include"
 format mdeutils "mbiff mauplay"
